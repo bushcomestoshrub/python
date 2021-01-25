2574b1ff29a7e204548c83de0c6bf2cbbf53df4d
bushcomestoshrub <garbajacct@gmail.com>
2021-01-24 17:52:50 -0700

    js basics

--- Makefile
+++ Makefile
@@ -2,5 +2,8 @@
 init:
 	pip install -r requirements.txt
 
+run:
+	python src/application.py
+
 test:
 	nosetests tests
--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,3 +1,17 @@
 //alert("Hello, World!");
 
-document.getElementById("jspara").innerHTML = "this is javascript from dir"
+document.getElementById("para").innerHTML = "this is javascript from dir"
+toggl = document.getElementById("tmp")
+
+
+function makeRed(element, color) {
+    element.style.color = color
+}
+
+function toggle() {
+    if (toggl.style.display === "none") {
+        toggl.style.display = "block";
+    } else {
+        toggl.style.display = "none";
+    }
+}
